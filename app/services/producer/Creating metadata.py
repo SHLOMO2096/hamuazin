6622339bbe334c84005580b1bb75273bffde2296
shlomo <shlomo2096@gmail.com>
2025-09-07 17:41:57 +0300

    Adding a function to create metadata

--- app/services/producer/Creating metadata.py
+++ app/services/producer/Creating metadata.py
@@ -1,9 +1,28 @@
-import pathlib
+from tinytag import TinyTag
 
+import json
 
+class MetadataCreator:
+    def __init__(self, file_path: str):
+        self.file_path = file_path
 
-def create_metadata_file(file_path: str, metadata: dict) -> None:
-    """Create a metadata file for a given file path."""
-    metadata_file = pathlib.Path(file_path).with_suffix(".meta.json")
-    with metadata_file.open("w") as f:
-        json.dump(metadata, f)
+    def get_audio_metadata(self) -> dict:
+        """
+
+        """
+        tag = TinyTag.get(self.file_path)
+        return {
+            "duration": tag.duration,
+            "sample_rate": tag.samplerate,
+            "bitrate": tag.bitrate,
+            "channels": tag.channels,
+            "filesize": tag.filesize,
+            "artist": tag.artist,
+            "album": tag.album,
+            "title": tag.title
+        }
+
+if __name__ == "__main__":
+    creator = MetadataCreator("C:\\Users\\shlomo\\Downloads\\podcasts\\download (1).wav")
+    metadata = creator.get_audio_metadata()
+    print(metadata)
